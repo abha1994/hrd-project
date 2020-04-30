d52c0a88020ac15a17ef26fe30e3bce9e0e857ee
abha1994 <abha.aquajar@gmail.com>
2020-04-30 21:34:23 +0530

    Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'HRD - Ministry of New and Renewable Energy') }}</title>
+	
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
@@ -18,7 +16,7 @@
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <title>Laravel</title>
+  
 
         <!-- Fonts -->
         
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,54 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+	<head>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-   <title>{{ config('app.name', 'Laravel') }}</title>
+	   <title>{{ config('app.name', 'HRD - Ministry of New and Renewable Energy') }}</title>
 
-  
- <script src="{{ asset('public/plugins/jquery/jquery.min.js') }}"></script>
+		<script src="{{ asset('public/plugins/jquery/jquery.min.js') }}"></script>
+		
+		<link rel="dns-prefetch" href="//fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-<!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>-->
-<!--link href="https://use.fontawesome.com/releases/v5.0.1/css/fontawesome.css" rel="stylesheet"-->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.css">
-    <!-- Styles -->
-    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
-</head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.css">
+		
+		<link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
+	</head>
+	<body class="hold-transition sidebar-mini">
+		<div class="wrapper">
 
-  @include('includes/partials/header')
+		  @include('includes/partials/header')
 
-  @auth
+		  @auth
 
-  @if (Auth::user()->scheme_code === 1)
-    @include('includes/partials/leftstudent')
-  @elseif (Auth::user()->scheme_code ==2)
-    @include('includes/partials/leftfellow')
-  @elseif (Auth::user()->scheme_code ==3)
-    @include('includes/partials/leftinst')
-  @else
-    @include('includes/partials/left')
-  @endif
-  
-  @endauth
-    
-  @yield('container') 
-   
-  @include('includes/partials/footer')
-  
-</div>
-<!-- ./wrapper -->
-
-<!-- <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
- -->
-
-
-</body>
+		  @if (Auth::user()->scheme_code === 1)
+			@include('includes/partials/leftstudent')
+		  @elseif (Auth::user()->scheme_code ==2)
+			@include('includes/partials/leftfellow')
+		  @elseif (Auth::user()->scheme_code ==3)
+			@include('includes/partials/leftinst')
+		  @else
+			@include('includes/partials/left')
+		  @endif
+		  
+		  @endauth
+			
+		  @yield('container') 
+		   
+		  @include('includes/partials/footer')
+		  
+		</div>
+	</body>
 </html>
